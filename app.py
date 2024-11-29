45e205ef536c1242d0df651abb8dbcb1c16bffb7
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-11-29 12:28:56 +0000

    commit fase 1 e 2

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import db, ListaSpesa
 #render_template: collegare file HTML.
 
 #inizializza l'app Flask
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
 lista_Spesa = []
 #rotta principale
 @app.route('/')
---
+++ models.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy() #inizialliziamo sqlAlchemy
+class ListaSpesa(db.Model): #tabella
+    id = db.Column(db.Integer, primary_key=True) #id elemento, unico  
+    elemento = db.Column(db.String(100), nullable=False) #elemento, non nullo
