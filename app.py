0b4497ba529a9a431b0aa40783873be41880ad8c
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-12-02 08:54:04 +0000

    quasissimamente finito

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with app.app_context():
 #rotta principale
 @app.route('/')
 def home():
-    lista_spesa = ListaSpesa.query.all() #COMMENTA
+    lista_spesa = ListaSpesa.query.all() #lista spesa diventa il contenuto del database
     return render_template('index.html', lista = lista_spesa)
     #visualizza
 
@@ -24,22 +24,22 @@ def home():
 def aggiungi():
     elemento = request.form['elemento']
     if elemento:
-        nuovo_elemento = ListaSpesa(elemento=elemento) #COMMENTA
-        db.session.add(nuovo_elemento) #COMMENTA
-        db.session.commit() #COMMENTA
+        nuovo_elemento = ListaSpesa(elemento=elemento) #nuovo_elemento lo eguagliamo all'elemento del database
+        db.session.add(nuovo_elemento) #aggiungi nel database l'argomento specificato
+        db.session.commit() #committa nel database
     return redirect(url_for('home'))
 
 @app.route('/rimuovi/<int:indice>', methods=['POST'])#usando il metodo post rimuoviamo un elemento dalla lista
 def rimuovi(indice):
-    elemento = ListaSpesa.query.get_or_404(indice) #COMMENTA
-    db.session.delete(elemento) #COMMENTA
-    db.session.commit() #COMMENTA
+    elemento = ListaSpesa.query.get_or_404(indice) #prende la query con l'indice specificato
+    db.session.delete(elemento) #elimina dal database quel specifico elemento
+    db.session.commit() #committa nel database
     return redirect(url_for('home'))
 
 @app.route('/svuota', methods=['POST'])
 def svuota():
-    ListaSpesa.query.delete() #COMMENTA ???
-    db.session.commit() #COMMENTA ???
+    ListaSpesa.query.delete() #svuota tutta la lista della spesa
+    db.session.commit() #committa nel database
     return redirect(url_for('home'))
 
 #avvio dell'app Flask
